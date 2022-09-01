f5fc2564130f30f5561cab03087240a5b967faf0
Justin Pham <justinpham79@outlook.com>
2022-09-01 00:52:08 -0700

    moved some notes. added script to run todomvc

--- Notes/Projects/todomvc.md
+++ Notes/Projects/todomvc.md
@@ -1,71 +1,61 @@
 **todomvc**\
 fork: https://github.com/Pjsrcool/todomvc
 
-These examples are already compiled. To run them, we need to open the `html` file in the web browser, or we need to serve the build folder. The following are how I ran each example.
+These examples are already compiled. To run them, we us a script from Madhurima.
 
+**To run each project**
+1. Make sure you are in this repo's root directory
+2. Run `node Utilities/serveTodo.js <directory>`
 
-# angular-dart 
-**Run Instructions**
-1. Open `html` file
+Replace `<directory>` with the absolute path to the example you want to run. Check the list below for the specific path used for each example. The beginning of each path is removed because it will be different on each person's computer. For example, if I wanted to run `angular2` on my computer, the command I would run is `node Utilities/serveTodo.js /home/justin/SCG-Research/Projects/todomvc/examples/angular2`
 
-**Notes**
-- Running `serve -s web` serves a site with wrong UI
-- Just running `serve` from the example root, then clicking `web/` serves a site with the correct UI, but does not save todo inputs.
-
-# angular2
-**Run Instructions**
-1. ??
-
-**Notes**
-- tried running `ng serve` on node 16
-- tried switching to nodejs 12 to install and run `npm run dev`
-
-# angular2_es2015
-# angularjs
-**Run Instructions**
-1. Open html file in example root
-
-# angularjs_require
-# aurelia
-# backbone
-# backbone_marionette
-# backbone_require
-# binding-scala
-# canjs
-# canjs_require
-# closure
-# cujo
-# dijon
-# dojo
-# duel
-# elm
-# emberjs
-# emberjs_require
-# enyo_backbone
-# exoskeleton
-# firebase-angular
-# gwt
-# jquery
-# js_of_ocaml
-# jsblacks
-# knockback
-# knockoutjs
-# knockoutjs_require
-# kotlin-react
-# lavaca_require
-# mithril
-# polymer
-# reactive
-# react
-# react-alt
-# react-backbone
-# react-hooks
-# reagent
-# riotjs
-# scalajs-react
-# typesript-angular
-# typescript-backbone
-# typescript-react
-# vanilla-es6
-# vanillajs
-# vue
+Project | Specific Directory
+--|--
+angular-dart | ?? `/todomvc/examples/angular-dart/web`
+angular2 | `/todomvc/examples/angular2`
+angular2_es2015
+angularjs
+angularjs_require
+aurelia
+backbone
+backbone_marionette
+backbone_require
+binding-scala
+canjs
+canjs_require
+closure
+cujo
+dijon
+dojo
+duel
+elm
+emberjs
+emberjs_require
+enyo_backbone
+exoskeleton
+firebase-angular
+gwt
+jquery
+js_of_ocaml
+jsblacks
+knockback
+knockoutjs
+knockoutjs_require
+kotlin-react
+lavaca_require
+mithril
+polymer
+reactive
+react
+react-alt
+react-backbone
+react-hooks
+reagent
+riotjs
+scalajs-react
+typesript-angular
+typescript-backbone
+typescript-react
+vanilla-es6
+vanillajs
+vue
---
+++ Utilities/serveTodo.js
@@ -0,0 +1,100 @@
+// const puppeteer = require('puppeteer');
+const fs = require('fs');
+// const waitForAnySelector = require('./helpers.js');
+var express = require('express');
+// const jsCov = require('./jsCoverage.js');
+var app = express();
+var path = require('path');
+var root = process.argv[2];
+console.log(root)
+var arr =((process.argv[2]).split(path.sep)).filter(el=> el!=="");
+var last = arr[arr.length-1] || arr[arr.length-2];
+//root=path.join(root,last)
+var isMain = (element,index) => arr[index].includes("todomvc") && arr[index+1]=="examples"
+var mainInd = arr.findIndex(isMain);
+var main = path.sep + path.join(...arr.slice(0,mainInd+1))+path.sep;
+var siteassets = path.sep+ path.join(main,"site-assets")+path.sep;
+//console.log(root,main,siteassets)
+app.use(express.static(root));
+app.use(express.static(main));
+app.use(express.static(siteassets));
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(root + '/index.html');
+});
+var server = app.listen(8080);
+
+/*
+
+(async () => {
+  if (process.argv.length < 3) {
+    console.log("Please provide argument the path to the local HTML file.")
+    return;
+  }
+  inPath = process.argv[2]
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  page.setDefaultNavigationTimeout( 90000 )
+  await Promise.all([
+    jsCov.startJSCov(page.coverage)
+  ]);
+  await page.goto('http://localhost:8080', {"waitUntil":["load", "networkidle2"]});
+  const selector = await waitForAnySelector(page, [
+    '#new-todo',
+    '.new-todo'
+  ])
+  for (var i = 0; i < 3; i++) {
+    await page.type(selector, 'Something to do ' + i).then(async () => { await page.keyboard.press('Enter') })
+  }
+
+  await page.click('.view label', { clickCount: 2 })
+  await page.type('.view label', " changed")
+  await page.keyboard.press('Enter')
+  const toggleAll = await waitForAnySelector(page, [
+    "label[for='toggle-all']",
+    "label[for='toggle-all']"
+  ])
+  await page.click(toggleAll);
+  await page.$$eval('.toggle', checks => {
+
+    //for (var i = 0; i < checks.length; i += 2)
+
+      checks[1].click();
+  })
+  await page.$$eval('.destroy', destroys => {
+    //for (var i = 0; i < destroys.length; i += 4)
+      destroys[2].click();
+  })
+  const aElementsCompleted = await page.$x("//a[contains(., 'Completed')]");
+  await aElementsCompleted[0].click();
+  const aElementsActive = await page.$x("//a[contains(., 'Active')]");
+  await aElementsActive[0].click();
+  const clearCompleted = await waitForAnySelector(page, [
+    '#clear-completed',
+    '.clear-completed'
+  ])
+
+  await page.click(clearCompleted);
+  const getAll = await waitForAnySelector(page, [
+    'a[href="#/"]',
+    'a[href="#!"]',
+    'a[href="#/all"]'
+  ])
+  await page.click(getAll);
+  const jsCoverage = await jsCov.stopJSCov(page.coverage);
+  await jsCov.parseJSCov(jsCoverage);
+  //await browser.close();
+  //server.close()
+})();
+
+
+*/
+
+/*
+Execution format:
+node scripts/todoPup.js /input/directory/
+Sample:
+node scripts/todoPup.js ../todomvcmaster/examples/vanillajs/index.html
+*/
